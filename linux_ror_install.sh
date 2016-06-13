4cc3cfbdf787dda3e16add8b06382e5fc63da97f
aaa <aaa@aaa.aaa>
2016-06-14 00:18:37 +0900

    test

---
+++ linux_ror_install.sh
@@ -0,0 +1,569 @@
+#!/bin/sh
+########################################################
+# Ruby On Rails 環境構築用シェル
+# 作成者  fukuda
+# 作成日  2016/01/06
+########################################################
+
+########################################################
+#  メニュー項目画面
+FNC_MENU(){
+  while true; do
+    cat << EOF
++-----------------------------------------+
+|                【 MENU 】         v 3.1 |
++-----------------------------------------+
+| [1]  開発用としてiptableとselinuxを解除 |
+| [2]  ruby をインストール                |
+| [3]  rails をインストール               |
+| [4]  node.js をインストール(テスト中)   |
+| [5]  ffmpeg をインストール(テスト中)    |
+| [6]  redis をインストール(テスト中)     |
+| [e]  シェルを終了                       |
++-----------------------------------------+
+EOF
+
+  #入力された項目を読み込み、変数KEYに代入する
+  read -p "項目を選択してください >>" KEY
+  case "${KEY}" in  #変数KEYに合った内容でコマンドが実行される
+    "1") FNC_ACTION ;;
+    "2") FNC_ACTION ;;
+    "3") FNC_ACTION ;;
+    "4") FNC_ACTION ;;
+    "5") FNC_ACTION ;;
+    "6") FNC_ACTION ;;
+    "e") break ;;
+    *) echo "($LINENO)  >> キーが違います。" ;;
+    esac
+    read -p "ENTERを押してください。" BLANK
+  done
+}
+
+########################################################
+# FNC_ACTION 共通メソッド
+FNC_ACTION(){
+  FNC_${KEY}
+  if [ $? != 0 ]; then
+     echo "($LINENO)  >> FNC_${KEY}で異常が発生しました"
+  fi
+}
+
+########################################################
+#  [1]
+FNC_1(){
+  echo "($LINENO) >> [1]  開発用としてiptableとselinuxを解除"
+  /etc/rc.d/init.d/iptables stop
+  chkconfig iptables off
+  chkconfig --list iptables
+  setenforce 0
+  # sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/sysconfig/selinux
+  chkconfig kdump off
+  echo "開発用パッケージをインストールします"
+  sudo yum -y groupinstall "Base" "Development tools"
+  return 0
+}
+
+########################################################
+#  [2]
+FNC_2(){
+  echo "($LINENO) >> [2]  ruby をインストール"
+  RUBY_INSTALL_SELECT
+  return 0
+}
+
+########################################################
+#  [3]
+FNC_3(){
+  echo "($LINENO) >> [3]  rails をインストール"
+  RUN_CHECK
+  RAILS_VERSION_CHECK
+  return 0
+}
+
+########################################################
+#  [4]
+FNC_4(){
+  echo "($LINENO) >> [4]  node.js をインストール(テスト中)"
+  RUN_CHECK
+  DEVELOP_PACKAGE_INSTALL
+  NODE_INSTALL_CHECK
+  return 0
+}
+
+########################################################
+#  [5]
+FNC_5(){
+  echo "($LINENO) >> [5]  ffmpeg をインストール(テスト中)"
+  RUN_CHECK
+  FFMPEG_INSTALL
+  # DEVELOP_PACKAGE_INSTALL
+  # NODE_INSTALL_CHECK
+  return 0
+}
+
+########################################################
+#  [6]
+FNC_6(){
+  echo "($LINENO) >> [6]  redis をインストール(テスト中)"
+  RUN_CHECK
+  REDIS_INSTALL
+  return 0
+}
+########################################################
+#  各種の呼び出し処理
+# 確認処理
+RUN_CHECK(){
+  while true; do
+    cat << EOF
++--------------------------------+
+| 実行しますが、よろしいですか？ |
++--------------------------------+
+| > [1] 実行                     |
+| > [2] 戻る                     |
++--------------------------------+
+EOF
+
+  read -p "項目を選択してください >>" KEY
+  case "${KEY}" in  #変数KEYに合った内容でコマンドが実行される
+    "1") break ;;
+    "2") FNC_MENU ;;
+    *) echo "($LINENO)  >> キーが違います。" ;;
+    esac
+  done
+}
+
+# 必要パッケージのインストール処理
+DEVELOP_PACKAGE_INSTALL(){
+  echo 'パッケージを最新にします。パスワードを聞かれることがあります。'
+  sudo -v
+  sudo yum update -y
+  sudo yum install -y zlib
+  sudo yum install -y zlib-devel
+  sudo yum install -y openssl-devel
+  sudo yum install -y sqlite-devel
+  sudo yum install -y gcc-c++
+  sudo yum install -y glibc-headers
+  sudo yum install -y libyaml-devel
+  sudo yum install -y readline
+  sudo yum install -y readline-devel
+  sudo yum install -y libffi-devel
+  sudo yum install -y libxml2
+  sudo yum install -y libxml2-devel
+  sudo yum install -y libxslt
+  sudo yum install -y libxslt-devel
+  sudo yum install -y libyaml-devel
+  sudo yum install -y make
+  sudo yum -y install yum-cron
+}
+
+RUBY_INSTALL_SELECT(){
+  while true; do
+    cat << EOF
++----------------------------+
+| どのアプリを利用しますか？ |
++----------------------------+
+| > [1] rvm                  |
+| > [2] rbenv                |
++----------------------------+
+EOF
+
+  read -p "項目を選択してください >>" KEY
+  case "${KEY}" in  #変数KEYに合った内容でコマンドが実行される
+    "1")
+      echo "($LINENO) >> [2]  rvm で rubyをインストール"
+      RUN_CHECK
+      DEVELOP_PACKAGE_INSTALL
+      RVM_INSTALL
+      RVM_RUBY_VERSION_CHECK
+      RVM_RUBY_INSTALL
+      break ;;
+    "2")
+      echo "($LINENO) >> [3]  rbenv で rubyをインストール"
+      RUN_CHECK
+      DEVELOP_PACKAGE_INSTALL
+      RBENV_INSTALL
+      RBENV_RUBY_VERSION_CHECK
+      RBENV_RUBY_INSTALL
+      break ;;
+    *) echo "($LINENO)  >> キーが違います。" ;;
+    esac
+  done
+}
+
+# rvmのインストール
+RVM_INSTALL(){
+  curl -L https://get.rvm.io | bash
+
+  # 設定を反映
+  source /etc/profile.d/rvm.sh
+  # sh /etc/profile.d/rvm.sh
+  # . /etc/profile.d/rvm.sh
+
+  # 必要パッケージ一式揃える
+  rvm requirements
+
+  echo 'rvm のバージョンは以下となります'
+  rvm -v
+}
+
+RVM_RUBY_VERSION_CHECK(){
+  while true; do
+    cat << EOF
++-----------------------------------+
+| Ruby のバージョンを確認しますか？ |
++-----------------------------------+
+| > [1] する                        |
+| > [2] しない                      |
++-----------------------------------+
+EOF
+
+  read -p "項目を選択してください >>" KEY
+  case "${KEY}" in  #変数KEYに合った内容でコマンドが実行される
+    "1") rvm list known ;;
+    "2") break ;;
+    *) echo "($LINENO)  >> キーが違います。" ;;
+    esac
+  done
+}
+
+RVM_RUBY_INSTALL(){
+  echo -n "バージョン : "
+  read ans
+  rvm install $ans
+  rvm use $ans --default
+
+  echo 'ruby のバージョンは以下となります'
+  ruby -v
+}
+
+# rbenvのインストール
+RBENV_INSTALL(){
+  sudo yum -y install git
+  git clone git://github.com/sstephenson/rbenv.git /usr/local/src/rbenv
+  echo 'export RBENV_ROOT="/usr/local/src/rbenv"' >> /etc/profile.d/rbenv.sh
+  echo 'export PATH="${RBENV_ROOT}/bin:${PATH}"' >> /etc/profile.d/rbenv.sh
+  echo 'eval "$(rbenv init -)"' >> /etc/profile.d/rbenv.sh
+  source /etc/profile.d/rbenv.sh
+  git clone git://github.com/sstephenson/ruby-build.git /usr/local/src/rbenv/plugins/ruby-build
+  ls /usr/local/src/rbenv/plugins/ruby-build/bin/
+
+  echo 'rbenv のバージョンは以下となります'
+  rbenv -v
+}
+
+RBENV_RUBY_VERSION_CHECK(){
+  while true; do
+    cat << EOF
++-----------------------------------+
+| Ruby のバージョンを確認しますか？ |
++-----------------------------------+
+| > [1] する                        |
+| > [2] しない                      |
++-----------------------------------+
+EOF
+
+  read -p "項目を選択してください >>" KEY
+  case "${KEY}" in  #変数KEYに合った内容でコマンドが実行される
+    "1") rbenv install --list ;;
+    "2") break ;;
+    *) echo "($LINENO)  >> キーが違います。" ;;
+    esac
+  done
+}
+
+RBENV_RUBY_INSTALL(){
+  echo -n "バージョン : "
+  read ans
+  rbenv install -v $ans
+  rbenv rehash
+  rbenv global $ans
+
+  echo 'ruby のバージョンは以下となります'
+  ruby -v
+}
+
+RAILS_VERSION_CHECK(){
+  while true; do
+    cat << EOF
+|------------------------------------------|
+| Rails のバージョンはどれを利用しますか？ |
+|------------------------------------------|
+| > [1] 3.2.22                             |
+| > [2] 4.0.13                             |
+| > [3] 4.1.15                             |
+| > [4] 4.2.6                              |
+| > [5] 最新                               |
+| > [6] バージョン検索                     |
+| > [7] 手動入力                           |
+|------------------------------------------|
+EOF
+
+  read -p "項目を選択してください >>" KEY
+  case "${KEY}" in  #変数KEYに合った内容でコマンドが実行される
+    "1")
+      RAILS_INSTALL 3.2.22
+      break ;;
+    "2")
+      RAILS_INSTALL 4.0.13
+      break ;;
+    "3")
+      RAILS_INSTALL 4.1.15
+      break ;;
+    "4")
+      RAILS_INSTALL 4.2.6
+      break ;;
+    "5")
+      gem install rails --pre
+      break ;;
+    "6")
+      gem query -ra -n  "^rails$" ;;
+    "7")
+      echo -n "バージョン : "
+      read ans
+      RAILS_INSTALL $ans
+      break ;;
+    *)
+      echo "($LINENO)  >> キーが違います。" ;;
+    esac
+  done
+}
+
+RAILS_INSTALL(){
+  gem install rack
+  gem install rails -v  $1
+
+  echo 'rails のバージョンは以下となります'
+  rails -v
+}
+
+NODE_INSTALL_CHECK(){
+  while true; do
+    cat << EOF
++----------------------------------------+
+| nodejsを何経由でインストールしますか？ |
++----------------------------------------+
+| > [1] yum                              |
+| > [2] nvm                              |
+| > [3] nodebrew                         |
++----------------------------------------+
+EOF
+
+  read -p "項目を選択してください >>" KEY
+  case "${KEY}" in  #変数KEYに合った内容でコマンドが実行される
+    "1")
+      sudo yum install -y epel-release
+      sudo yum install -y nodejs
+      # yum install -y npm --enablerepo=epel
+      node -v
+      break ;;
+    "2")
+      NVM_INSTALL
+      NVM_RUBY_VERSION_CHECK
+      NVM_RUBY_INSTALL
+      break ;;
+    "3")
+      NODEBLEW_INSTALL
+      NODEBLEW_RUBY_VERSION_CHECK
+      NODEBLEW_RUBY_INSTALL
+      break ;;
+    *) echo "($LINENO)  >> キーが違います。" ;;
+    esac
+  done
+}
+
+NVM_INSTALL(){
+  sudo yum -y install git
+  git clone git://github.com/creationix/nvm.git ~/.nvm
+  source ~/.nvm/nvm.sh
+  nvm version
+}
+
+NVM_RUBY_VERSION_CHECK(){
+  while true; do
+    cat << EOF
++----------------------------------+
+| node のバージョンを確認しますか？|
++----------------------------------+
+| > [1] する                       |
+| > [2] しない                     |
++----------------------------------+
+EOF
+
+  read -p "項目を選択してください >>" KEY
+  case "${KEY}" in  #変数KEYに合った内容でコマンドが実行される
+    "1") nvm ls-remote ;;
+    "2") break ;;
+    *) echo "($LINENO)  >> キーが違います。" ;;
+    esac
+  done
+}
+
+NVM_RUBY_INSTALL(){
+  echo -n "バージョン(vは削除して記入) : "
+  read ans
+  nvm install $ans
+  nvm alias default v$ans
+  # yum install -y npm
+
+  echo 'node のバージョンは以下となります'
+  node -v
+}
+
+NODEBLEW_INSTALL(){
+  curl -L git.io/nodebrew | perl - setup
+  export PATH=$HOME/.nodebrew/current/bin:$PATH
+  source ~/.bashrc
+  nodebrew help
+}
+
+NODEBLEW_RUBY_VERSION_CHECK(){
+  while true; do
+    cat << EOF
++----------------------------------+
+| node のバージョンを確認しますか？|
++----------------------------------+
+| > [1] する                       |
+| > [2] しない                     |
++----------------------------------+
+EOF
+
+  read -p "項目を選択してください >>" KEY
+  case "${KEY}" in  #変数KEYに合った内容でコマンドが実行される
+    "1") nodebrew ls-remote ;;
+    "2") break ;;
+    *) echo "($LINENO)  >> キーが違います。" ;;
+    esac
+  done
+}
+
+NODEBLEW_RUBY_INSTALL(){
+  echo -n "バージョン(vは削除して記入) : "
+  read ans
+  nodebrew install-binary v$ans
+  nodebrew use v$ans
+  # yum install -y npm
+
+  echo 'node のバージョンは以下となります'
+  node -v
+}
+
+FFMPEG_INSTALL(){
+  # パッケージ
+  yum update -y
+  yum install autoconf automake cmake freetype-devel gcc gcc-c++ git libtool make mercurial nasm pkgconfig zlib-devel -y
+
+  # source code new directory to put all of the
+  mkdir ~/ffmpeg_sources
+
+  # Yasm
+  cd ~/ffmpeg_sources
+  git clone --depth 1 git://github.com/yasm/yasm.git
+  cd yasm
+  autoreconf -fiv
+  ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin"
+  make
+  make install
+  make distclean
+
+  # libx264
+  cd ~/ffmpeg_sources
+  git clone --depth 1 git://git.videolan.org/x264
+  cd x264
+  PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --enable-static
+  make
+  make install
+  make distclean
+
+  # libx265
+  cd ~/ffmpeg_sources
+  hg clone https://bitbucket.org/multicoreware/x265
+  cd ~/ffmpeg_sources/x265/build/linux
+  cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$HOME/ffmpeg_build" -DENABLE_SHARED:bool=off ../../source
+  make
+  make install
+
+  # libfdk_aac
+  cd ~/ffmpeg_sources
+  git clone --depth 1 git://git.code.sf.net/p/opencore-amr/fdk-aac
+  cd fdk-aac
+  autoreconf -fiv
+  ./configure --prefix="$HOME/ffmpeg_build" --disable-shared
+  make
+  make install
+  make distclean
+
+  # libmp3lame
+  cd ~/ffmpeg_sources
+  curl -L -O http://downloads.sourceforge.net/project/lame/lame/3.99/lame-3.99.5.tar.gz
+  tar xzvf lame-3.99.5.tar.gz
+  cd lame-3.99.5
+  ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --disable-shared --enable-nasm
+  make
+  make install
+  make distclean
+
+  # libopus
+  cd ~/ffmpeg_sources
+  git clone git://git.opus-codec.org/opus.git
+  cd opus
+  autoreconf -fiv
+  ./configure --prefix="$HOME/ffmpeg_build" --disable-shared
+  make
+  make install
+  make distclean
+
+  # libogg
+  cd ~/ffmpeg_sources
+  curl -O http://downloads.xiph.org/releases/ogg/libogg-1.3.2.tar.gz
+  tar xzvf libogg-1.3.2.tar.gz
+  cd libogg-1.3.2
+  ./configure --prefix="$HOME/ffmpeg_build" --disable-shared
+  make
+  make install
+  make distclean
+
+  # libvorbis
+  cd ~/ffmpeg_sources
+  curl -O http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.4.tar.gz
+  tar xzvf libvorbis-1.3.4.tar.gz
+  cd libvorbis-1.3.4
+  LDFLAGS="-L$HOME/ffmeg_build/lib" CPPFLAGS="-I$HOME/ffmpeg_build/include" ./configure --prefix="$HOME/ffmpeg_build" --with-ogg="$HOME/ffmpeg_build" --disable-shared
+  make
+  make install
+  make distclean
+
+  # libvpx
+  cd ~/ffmpeg_sources
+  git clone --depth 1 https://chromium.googlesource.com/webm/libvpx.git
+  cd libvpx
+  ./configure --prefix="$HOME/ffmpeg_build" --disable-examples
+  make
+  make install
+  make clean
+
+  # FFmpeg
+  cd ~/ffmpeg_sources
+  git clone --depth 1 git://source.ffmpeg.org/ffmpeg
+  cd ffmpeg
+  PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure --prefix="$HOME/ffmpeg_build" --extra-cflags="-I$HOME/ffmpeg_build/include" --extra-ldflags="-L$HOME/ffmpeg_build/lib" --bindir="$HOME/bin" --pkg-config-flags="--static" --enable-gpl --enable-nonfree --enable-libfdk-aac --enable-libfreetype --enable-libmp3lame --enable-libopus --enable-libvorbis --enable-libvpx --enable-libx264 --enable-libx265
+  make
+  make install
+  make distclean
+
+  ffmpeg -version
+}
+
+REDIS_INSTALL(){
+  wget http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+  wget http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
+  sudo rpm -Uvh remi-release-6*.rpm epel-release-6*.rpm
+  sudo yum --enablerepo=remi,epel install redis -y
+  sudo service redis start
+  sudo chkconfig redis on
+
+  echo 'redis のバージョンは以下となります'
+  redis-server --version
+}
+########################################################
+#  エンド処理
+FNC_MENU      #関数FNC_MENUを呼ぶ
---
+++ rails_menu.sh
@@ -0,0 +1,581 @@
+
+########################################################
+# Rails メニュー
+# 作成者  fukuda
+# 作成日  2015/10/27
+########################################################
+
+########################################################
+# railsのディレクトリ
+rails_dir="/var/www/rails/"
+# rails_app名
+app_name="TYM"
+# rails_appディレクトリ
+rails_app_dir=$rails_dir$app_name
+
+########################################################
+# mysqlデータのバックアップ数
+log_cnt_max=10
+
+# mysqlデータのバックアップ先
+mysql_bk_dr="/var/www/rails/mysql_bk/"
+
+# mysql development user
+mysql_development_user="root"
+# mysql development pass
+mysql_development_pass=""
+
+# mysql production user
+mysql_production_user="root"
+# mysql production pass
+mysql_production_pass=""
+
+########################################################
+# unicorn_pid
+unicorn_pid="$rails_app_dir/tmp/pids/unicorn.pid"
+
+########################################################
+#  メニュー項目画面
+########################################################
+DEV_MENU()
+{
+  while true; do
+    cat << EOF
++--------------------------------+
+| 【 RAILS MENU dev_mode】 v 2.0 |
++--------------------------------+
+| [1]  nginx + unicorn 停止      |
+| [2]  nginx + unicorn 起動      |
+| [3]  DB + テーブル 構築        |
+| [4]  DB + テーブル 再構築      |
+| [5]  キャッシュの削除          |
+| [6]  ルートファイルの確認      |
+| [7]  bundle install 実行       |
+| [8]  bundle install 再実行     |
+| [9]  rails console 実行        |
+| [e]  シェルを終了              |
++--------------------------------+
+EOF
+
+
+
+	read -p "項目を選択してください >>" KEY  #入力された項目を読み込み、変数KEYに代入する
+	case "${KEY}" in  #変数KEYに合った内容でコマンドが実行される
+		"1") FNC_ACTION ;;
+		"2") FNC_ACTION ;;
+		"3") FNC_ACTION ;;
+		"4") FNC_ACTION ;;
+		"5") FNC_ACTION ;;
+		"6") FNC_ACTION ;;
+		"7") FNC_ACTION ;;
+		"8") FNC_ACTION ;;
+		"9") FNC_ACTION ;;
+		"e") break ;;
+		"100") FNC_ACTION ;;
+		"101") FNC_ACTION ;;
+		"102") FNC_ACTION ;;
+		"103") FNC_ACTION ;;
+		"104") FNC_ACTION ;;
+		"105") FNC_ACTION ;;
+		"201") FNC_ACTION ;;
+		"202") FNC_ACTION ;;
+		"203") FNC_ACTION ;;
+		"204") FNC_ACTION ;;
+		"205") FNC_ACTION ;;
+		*) echo "($LINENO)	>> キーが違います。" ;;
+
+		esac
+
+		read -p "ENTERを押してください。" BLANK
+
+	done
+}
+
+########################################################
+# FNC_ACTION 共通メソッド
+FNC_ACTION(){
+	FNC_${KEY}
+	if [ $? != 0 ]; then
+		 echo "($LINENO)	>> FNC_${KEY}で異常が発生しました"
+	fi
+}
+
+########################################################
+#  [1]  nginx + unicorn 停止
+FNC_1(){
+	echo "($LINENO)	>> nginx + unicorn 停止"
+	NGINX_STOP
+	DEV_LOG_BK
+	CACHE_CREAE
+	return 0   #正常終了は戻り値が0
+}
+
+########################################################
+#  [2]  nginx + unicorn 起動
+FNC_2(){
+	echo "($LINENO)	>> nginx + unicorn 起動"
+	NGINX_DEV_START
+	return 0   #正常終了は戻り値が0
+}
+
+########################################################
+#  [3]  DB + テーブル 構築
+FNC_3(){
+	echo "($LINENO)	>> DB + テーブル 構築"
+	DB_DEV_MIGRATE
+	return 0
+}
+
+########################################################
+#  [4]  DB + テーブル 再構築
+FNC_4(){
+	echo "($LINENO)	>> [4]  DB + テーブル 再構築"
+
+	# SQLバックアップ
+	# ALL_MYSQL_BK
+	DEV_MYSQL_BK
+
+	echo "($LINENO)	>> DB 再構築"
+	bundle exec rake db:drop:all
+
+  # 開発DB作成
+  DB_DEV_MIGRATE
+
+	return 0
+}
+
+########################################################
+# [5]  キャッシュの削除
+FNC_5(){
+	echo "($LINENO)	>> キャッシュの削除を行ないます"
+	DEV_LOG_BK
+	CACHE_CREAE
+	return 0
+}
+
+########################################################
+# [6]  ルートファイルの確認
+FNC_6(){
+	echo "($LINENO)	>> ルート を表示させます"
+	bundle exec rake routes
+	return 0
+}
+
+########################################################
+# [7]  bundle install 実行
+FNC_7(){
+	echo "($LINENO)	>> bundle install 実行します"
+	# bundle exec bundle install --path vendor/bundle
+	bundle install --path vendor/bundle
+	return 0
+}
+
+########################################################
+# [8]  bundle install 再実行
+FNC_8(){
+	echo "($LINENO)	>> bundle install 再実行します"
+	sudo rm -rf vendor/bundle
+	# bundle exec bundle install --path vendor/bundle
+	bundle install --path vendor/bundle
+	return 0
+}
+
+########################################################
+# [9]  rails console 実行
+FNC_9(){
+	echo "($LINENO)	>> rails console 実行します"
+	bundle exec rails console
+	return 0
+}
+
+########################################################
+# [100] nginx + unicorn 停止
+FNC_100(){
+	echo "($LINENO)	>> 本番環境 nginx + unicorn 停止"
+	NGINX_STOP
+
+	return 0
+}
+
+########################################################
+# [101] nginx + unicorn 起動
+FNC_101(){
+	echo "($LINENO)	>> 本番環境 nginx + unicorn 起動"
+	PRO_LOG_BK
+	NGINX_PRO_START
+	return 0
+}
+
+########################################################
+# [102] DB + テーブル 構築
+FNC_102(){
+	echo "($LINENO)	>> 本番環境 テーブル 構築"
+	PRO_LOG_BK
+	DB_PRO_MIGRATE
+	return 0
+}
+
+########################################################
+# [103] キャッシュの削除
+FNC_103(){
+	echo "($LINENO)	>> キャッシュの削除を行ないます"
+	PRO_LOG_BK
+	CACHE_CREAE
+	return 0
+}
+
+########################################################
+# [104] コンパイル
+FNC_104(){
+	PRO_LOG_BK
+	CACHE_CREAE
+	PRECOMPILE
+	return 0
+}
+
+########################################################
+# [105] DB + テーブル 再構築
+FNC_105(){
+	echo "($LINENO)	>> 本番環境 テーブル 再構築"
+
+	# ログのバックアップ
+	PRO_LOG_BK
+
+	# 全体バックアップ
+	# ALL_MYSQL_BK
+	PRO_MYSQL_BK
+
+	bundle exec rake db:migrate:reset RAILS_ENV=production
+
+	echo "($LINENO)	>> seed用yml 実行（heart_seed）"
+	CATALOGS=production bundle exec rake db:seed RAILS_ENV=production
+
+	# 復元はこちら
+	# mysql -u root -p < dump.sql
+
+	return 0
+}
+########################################################
+
+########################################################
+# rails_best_practices 実行
+FNC_201(){
+	echo "($LINENO)	>> rails_best_practices 実行します"
+	bundle exec rails_best_practices -f html .
+
+	return 0   #正常終了は戻り値が0
+}
+########################################################
+
+########################################################
+# delayed_job 再起動
+FNC_202(){
+	echo "($LINENO)	>> delayed_job 停止"
+	bundle exec bin/delayed_job stop
+
+	echo "($LINENO)	>> delayed_job ジョブクリア"
+	bundle exec rake jobs:clear
+
+	echo "($LINENO)	>> delayed_job 起動"
+	bundle exec bin/delayed_job start
+
+	return 0   #正常終了は戻り値が0
+}
+########################################################
+
+########################################################
+# i18n_generators 実行
+FNC_203(){
+	echo "($LINENO)	>> i18n_generators 実行"
+	bundle exec rails g i18n ja
+
+	return 0   #正常終了は戻り値が0
+}
+########################################################
+
+########################################################
+# whenever 実行
+FNC_204(){
+	echo "($LINENO)	>> whenever 削除"
+	bundle exec whenever --clear-crontab
+	echo "($LINENO)	>> whenever 登録"
+	bundle exec whenever --update-crontab
+}
+########################################################
+
+########################################################
+# bower install
+FNC_205(){
+	echo "($LINENO)	>> DB + テーブル 構築"
+	BOWER_INSTALL
+	return 0
+}
+
+########################################################
+# 共通メソッド
+# nginx 停止
+NGINX_STOP(){
+	echo "($LINENO)	>> Nginx 停止"
+	 service nginx stop
+
+	echo "($LINENO)	>> Unicorn 停止"
+	 kill -QUIT `cat $unicorn_pid`
+}
+
+# nginx 起動（開発環境）
+NGINX_DEV_START(){
+	# /etc/rc.d/init.d/httpd start
+	echo "($LINENO)	>> Unicorn 起動"
+	bundle exec unicorn_rails -c config/unicorn.rb -D
+
+	# unicorn単体用
+	# bundle exec unicorn_rails -c config/unicorn.rb -p 8080 -D
+
+	echo "($LINENO)	>> nginx 起動"
+	 service nginx start
+}
+
+# nginx 起動（開発環境）
+NGINX_PRO_START(){
+	export SECRET_KEY_BASE='bundle exec rake secret'
+
+	echo "($LINENO)	>> Unicorn 起動"
+	# bundle exec unicorn -E production -c config/unicorn.rb -D
+	SECRET_KEY_BASE=$(bundle exec rake secret) bundle exec unicorn -D -c config/unicorn.rb -E production
+
+	echo "($LINENO)	>> nginx 起動"
+	 service nginx start
+}
+
+# コンパイル
+PRECOMPILE(){
+	echo "($LINENO)	>> 古いコンパイルファイル 削除"
+	###  rm -rf public/assets
+	bundle exec rake assets:clean RAILS_ENV=production
+
+	echo "($LINENO)	>> assets コンパイル 実行"
+	bundle exec rake assets:precompile RAILS_ENV=production
+}
+
+# 開発用ログをバックアップ
+DEV_LOG_BK(){
+	echo "($LINENO)	>> development ログ バックアップ"
+	 cp -p $rails_app_dir/log/development.log $rails_app_dir/log/development.log_$(date +%Y%m%d%H%M%S)
+
+	# log_bk del ##########################
+	all_file="*"
+
+	cd $rails_dir$app_name/log
+
+	# development
+	rails_env_bk="development.log_"
+	cnt=` ls $rails_app_dir/log/$rails_env_bk$all_file | wc -l`
+
+	#ファイル数が設定値より大きいか比較
+	if [ $cnt -gt $log_cnt_max ]; then
+		echo "($LINENO)	>> development ログ バックアップ 過剰分削除"
+		 ls -tr $rails_app_dir/log/$rails_env_bk$all_file | head -$(($cnt-$log_cnt_max)) | xargs rm -f
+	else
+		echo "($LINENO)	>> development ログ バックアップ 保存"
+	fi
+
+	cd $rails_dir$app_name
+
+	echo "($LINENO)	>> ログ 初期化"
+	bundle exec rake log:clear
+}
+
+# 本番用ログをバックアップ
+PRO_LOG_BK(){
+	echo "($LINENO)	>> production ログ バックアップ"
+	 cp -p $rails_app_dir/log/production.log $rails_app_dir/log/production.log_$(date +%Y%m%d%H%M%S)
+
+	# log_bk del ##########################
+	all_file="*"
+
+	cd $rails_dir$app_name/log
+
+	# production
+	rails_env_bk="production.log_"
+	cnt=` ls $rails_app_dir/log/$rails_env_bk$all_file | wc -l`
+
+	#ファイル数が設定値より大きいか比較
+	if [ $cnt -gt $log_cnt_max ]; then
+		echo "($LINENO)	>> production ログ バックアップ 過剰分削除"
+		 ls -tr $rails_app_dir/log/$rails_env_bk$all_file | head -$(($cnt-$log_cnt_max)) | xargs rm -f
+	else
+		echo "($LINENO)	>> production ログ バックアップ 保存"
+	fi
+
+	cd $rails_dir$app_name
+
+	echo "($LINENO)	>> ログ 初期化"
+	bundle exec rake log:clear
+}
+
+# 開発環境DB、テーブル構築
+DB_DEV_MIGRATE(){
+	bundle exec rake db:create:all
+	bundle exec rake db:migrate
+  echo "($LINENO)	>> annotate（gem） 起動"
+	bundle exec annotate
+
+	echo "($LINENO)	>> seed用ymlファイル 削除（heart_seed）"
+	rm -r -f $rails_app_dir/db/seeds/*.yml
+
+	echo "($LINENO)	>> seed用ymlファイル 作成（heart_seed）"
+	bundle exec rake heart_seed:xls
+
+	echo "($LINENO)	>> seedファイル 実行"
+	bundle exec rake db:seed
+}
+
+# 本番環境DB＋テーブル構築
+DB_PRO_MIGRATE(){
+	bundle exec rake db:create:all
+	bundle exec rake db:migrate RAILS_ENV=production
+
+  echo "($LINENO)	>> seed用ymlファイル 削除（heart_seed）"
+	rm -r -f $rails_app_dir/db/seeds/*.yml
+
+	echo "($LINENO)	>> seed用ymlファイル 作成（heart_seed）"
+	bundle exec rake heart_seed:xls
+
+	echo "($LINENO)	>> seedファイル 実行"
+	bundle exec rake db:seed
+}
+
+# キャッシュクリア
+CACHE_CREAE(){
+	echo "($LINENO)	>> キャッシュ クリア"
+	bundle exec rake tmp:clear
+}
+
+# bower インストール
+BOWER_INSTALL(){
+	echo "($LINENO)	>> bower install"
+	rake bower:install['--allow-root']
+}
+
+# # 全てのDBデータをバックアップ
+# ALL_MYSQL_BK(){
+# 	echo "($LINENO)	>> DBのバックアップファイルを構築します。"
+# 	if [ -e ../mysql_bk ]; then
+# 		# フォルダがある場合
+# 		echo "($LINENO)	>> フォルダが存在します"
+# 	else
+# 		# フォルダがない場合
+# 		mkdir ../mysql_bk
+# 	fi
+
+# 	if [ -e $mysql_bk_dr_cp ]; then
+# 		# フォルダがある場合
+# 		echo "($LINENO)	>> フォルダが存在します"
+# 	else
+# 		# フォルダがない場合
+# 		mkdir -p $mysql_bk_dr_cp
+# 	fi
+
+# 	# 全体##########################
+# 	fn="/mysql_bk_"
+# 	all_file="*"
+
+# 	 mysqldump --user=$mysql_development_user --password="$mysql_development_pass" -x --all-databases > $mysql_bk_dr/mysql_bk_$(date +%Y%m%d%H%M%S).sql
+
+# 	 ls $mysql_bk_dr$fn$all_file | wc -l
+
+# 	cnt=` ls $mysql_bk_dr$fn$all_file | wc -l`
+# 	echo "($LINENO)	>> 全体のバックアップ数 = $cnt"
+
+# 	cd $mysql_bk_dr
+# 	#ファイル数が設定値より大きいか比較
+# 	if [ $cnt -gt $log_cnt_max ]; then
+# 		echo "($LINENO)	>> all_DB 削除"
+# 		 ls -tr $mysql_bk_dr | head -$(($cnt-$log_cnt_max)) | xargs rm -f
+# 	else
+# 		echo "($LINENO)	>> all_DB 保存"
+# 	fi
+
+# 	cd $rails_dir$app_name
+
+# 	 rm -rf $mysql_bk_dr_cp/*
+# 	cp -r -f $mysql_bk_dr $mysql_bk_dr_cp
+
+
+# 	# 復元はこちら
+# 	# mysql -u root -p < dump.sql
+# }
+
+# 開発環境のDBバックアップ
+DEV_MYSQL_BK(){
+	echo "($LINENO)	>> DBのバックアップファイルを構築します。"
+	if [ -e ../mysql_bk ]; then
+		# フォルダがある場合
+		echo "($LINENO)	>> フォルダが存在します"
+	else
+		# フォルダがない場合
+		mkdir ../mysql_bk
+	fi
+
+	# development ##########################
+	rails_env="_development"
+	rails_env_bk="_mysql_bk_"
+	all_file="*"
+
+	mysqldump --user=$mysql_development_user --password="$mysql_development_pass"  $app_name$rails_env > $mysql_bk_dr/$app_name$rails_env$rails_env_bk$(date +%Y%m%d%H%M%S).sql
+
+	cnt=` ls $mysql_bk_dr/$app_name$rails_env$rails_env_bk$all_file | wc -l`
+
+	echo "バックアップ数 = $cnt"
+	echo "バックアップ設定数 = $log_cnt_max"
+
+	#ファイル数が設定値より大きいか比較
+	cd $mysql_bk_dr
+	if [ $cnt -gt $log_cnt_max ]; then
+		echo "($LINENO)	>> development_db 削除"
+		ls -tr $mysql_bk_dr/$app_name$rails_env$rails_env_bk$all_file | head -$(($cnt-$log_cnt_max)) | xargs rm -rf
+	else
+		echo "($LINENO)	>> development_db 保存"
+	fi
+	cd $rails_dir$app_name
+}
+
+# 開発環境DBバックアップ
+PRO_MYSQL_BK(){
+	echo "($LINENO)	>> DBのバックアップファイルを構築します。"
+	if [ -e ../mysql_bk ]; then
+		# フォルダがある場合
+		echo "($LINENO)	>> フォルダが存在します"
+	else
+		# フォルダがない場合
+		mkdir ../mysql_bk
+	fi
+
+	# production ##########################
+	rails_env="_production"
+	rails_env_bk="_mysql_bk_"
+	all_file="*"
+
+	 mysqldump --user=$mysql_production_user --password="$mysql_production_pass"  $app_name$rails_env > $mysql_bk_dr/$app_name$rails_env$rails_env_bk$(date +%Y%m%d%H%M%S).sql
+
+	cnt=` ls $mysql_bk_dr/$app_name$rails_env$rails_env_bk$all_file | wc -l`
+
+	echo "バックアップ数 = $cnt"
+	echo "バックアップ設定数 = $log_cnt_max"
+
+	#ファイル数が設定値より大きいか比較
+	cd $mysql_bk_dr
+	if [ $cnt -gt $log_cnt_max ]; then
+		echo "($LINENO)	>> development_db 削除"
+		ls -tr $mysql_bk_dr/$app_name$rails_env$rails_env_bk$all_file | head -$(($cnt-$log_cnt_max)) | xargs rm -rf
+	else
+		echo "($LINENO)	>> development_db 保存"
+	fi
+	cd $rails_dir$app_name
+}
+
+
+########################################################
+#   メイン
+########################################################
+DEV_MENU      #関数DEV_MENUを呼ぶ
