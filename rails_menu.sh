56f45dbab15797e92101121eefd38c92b3a5ef34
fukuda <fukuda.hiroaki@k-idea.jp>
2016-06-27 19:27:11 +0900

    メニュー追加中

--- rails_menu.sh
+++ rails_menu.sh
@@ -28,6 +28,7 @@ unicorn_pid="$rails_app_dir/tmp/pids/unicorn.pid"
 
 ########################################################
 # TOPメニュー
+########################################################
 TOP_MENU()
 {
   while true; do
@@ -86,15 +87,15 @@ EOF
 
 	read -p "項目を選択してください >>" KEY  #入力された項目を読み込み、変数KEYに代入する
 	case "${KEY}" in  #変数KEYに合った内容でコマンドが実行される
-		"1") FNC_ACTION ;;
-		"2") FNC_ACTION ;;
-		"3") FNC_ACTION ;;
-		"4") FNC_ACTION ;;
-		"5") FNC_ACTION ;;
-		"6") FNC_ACTION ;;
-		"7") FNC_ACTION ;;
-		"8") FNC_ACTION ;;
-		"9") FNC_ACTION ;;
+		"1") DEV_ACTION ;;
+		"2") DEV_ACTION ;;
+		"3") DEV_ACTION ;;
+		"4") DEV_ACTION ;;
+		"5") DEV_ACTION ;;
+		"6") DEV_ACTION ;;
+		"7") DEV_ACTION ;;
+		"8") DEV_ACTION ;;
+		"9") DEV_ACTION ;;
 		"e") break ;;
 		*) echo "($LINENO)	>> キーが違います。" ;;
 		esac
@@ -102,18 +103,16 @@ EOF
 	done
 }
 
-########################################################
-# FNC_ACTION 共通メソッド
-FNC_ACTION(){
-	FNC_${KEY}
+# DEV_ACTION 共通メソッド
+DEV_ACTION(){
+	DEV_MENU_${KEY}
 	if [ $? != 0 ]; then
-		 echo "($LINENO)	>> FNC_${KEY}で異常が発生しました"
+		 echo "($LINENO)	>> DEV_MENU_${KEY}で異常が発生しました"
 	fi
 }
 
-########################################################
 #  [1]  nginx + unicorn 停止
-FNC_1(){
+DEV_MENU_1(){
 	echo "($LINENO)	>> nginx + unicorn 停止"
 	NGINX_STOP
 	DEV_LOG_BK
@@ -121,25 +120,22 @@ FNC_1(){
 	return 0   #正常終了は戻り値が0
 }
 
-########################################################
 #  [2]  nginx + unicorn 起動
-FNC_2(){
+DEV_MENU_2(){
 	echo "($LINENO)	>> nginx + unicorn 起動"
 	NGINX_DEV_START
 	return 0   #正常終了は戻り値が0
 }
 
-########################################################
 #  [3]  DB + テーブル 構築
-FNC_3(){
+DEV_MENU_3(){
 	echo "($LINENO)	>> DB + テーブル 構築"
 	DB_DEV_MIGRATE
 	return 0
 }
 
-########################################################
 #  [4]  DB + テーブル 再構築
-FNC_4(){
+DEV_MENU_4(){
 	echo "($LINENO)	>> [4]  DB + テーブル 再構築"
 
 	# SQLバックアップ
@@ -154,35 +150,31 @@ FNC_4(){
 	return 0
 }
 
-########################################################
 # [5]  キャッシュの削除
-FNC_5(){
+DEV_MENU_5(){
 	echo "($LINENO)	>> キャッシュの削除を行ないます"
 	DEV_LOG_BK
 	CACHE_CREAE
 	return 0
 }
 
-########################################################
 # [6]  ルートファイルの確認
-FNC_6(){
+DEV_MENU_6(){
 	echo "($LINENO)	>> ルート を表示させます"
 	bundle exec rake routes
 	return 0
 }
 
-########################################################
 # [7]  bundle install 実行
-FNC_7(){
+DEV_MENU_7(){
 	echo "($LINENO)	>> bundle install 実行します"
 	# bundle exec bundle install --path vendor/bundle
 	bundle install --path vendor/bundle
 	return 0
 }
 
-########################################################
 # [8]  bundle install 再実行
-FNC_8(){
+DEV_MENU_8(){
 	echo "($LINENO)	>> bundle install 再実行します"
 	sudo rm -rf vendor/bundle
 	# bundle exec bundle install --path vendor/bundle
@@ -190,16 +182,16 @@ FNC_8(){
 	return 0
 }
 
-########################################################
 # [9]  rails console 実行
-FNC_9(){
+DEV_MENU_9(){
 	echo "($LINENO)	>> rails console 実行します"
 	bundle exec rails console
 	return 0
 }
 
- #######################################################
-# 共通メソッド
+#######################################################
+# 呼び出しメソッド
+#######################################################
 # nginx 停止
 NGINX_STOP(){
 	echo "($LINENO)	>> Nginx 停止"
@@ -262,6 +254,54 @@ DEV_LOG_BK(){
 	bundle exec rake log:clear
 }
 
+# 本番用ログをバックアップ
+PRO_LOG_BK(){
+	echo "($LINENO)	>> production ログ バックアップ"
+	 cp -p $rails_app_dir/log/production.log $rails_app_dir/log/production.log_$(date +%Y%m%d%H%M%S)
+
+	# log_bk del ##########################
+	all_file="*"
+
+	cd $rails_dir$app_name/log
+
+	# production
+	rails_env_bk="production.log_"
+	cnt=` ls $rails_app_dir/log/$rails_env_bk$all_file | wc -l`
+
+	#ファイル数が設定値より大きいか比較
+	if [ $cnt -gt $log_cnt_max ]; then
+		echo "($LINENO)	>> production ログ バックアップ 過剰分削除"
+		 ls -tr $rails_app_dir/log/$rails_env_bk$all_file | head -$(($cnt-$log_cnt_max)) | xargs rm -f
+	else
+		echo "($LINENO)	>> production ログ バックアップ 保存"
+	fi
+
+	cd $rails_dir$app_name
+
+	echo "($LINENO)	>> ログ 初期化"
+	bundle exec rake log:clear
+}
+
+
+# キャッシュクリア
+CACHE_CREAE(){
+	echo "($LINENO)	>> キャッシュ クリア"
+	bundle exec rake tmp:clear
+}
+
+# コンパイル
+PRECOMPILE(){
+	echo "($LINENO)	>> 古いコンパイルファイル 削除"
+	###  rm -rf public/assets
+	bundle exec rake assets:clean RAILS_ENV=production
+
+	echo "($LINENO)	>> assets コンパイル 実行"
+	bundle exec rake assets:precompile RAILS_ENV=production
+}
+
+
+
+
 # 開発環境DB、テーブル構築
 DB_DEV_MIGRATE(){
 	bundle exec rake db:create:all
@@ -271,11 +311,21 @@ DB_DEV_MIGRATE(){
 	bundle exec rake db:seed
 }
 
-# キャッシュクリア
-CACHE_CREAE(){
-	echo "($LINENO)	>> キャッシュ クリア"
-	bundle exec rake tmp:clear
+# 本番環境DB＋テーブル構築
+DB_PRO_MIGRATE(){
+	bundle exec rake db:create:all
+	bundle exec rake db:migrate RAILS_ENV=production
+
+  echo "($LINENO)	>> seed用ymlファイル 削除（heart_seed）"
+	rm -r -f $rails_app_dir/db/seeds/*.yml
+
+	echo "($LINENO)	>> seed用ymlファイル 作成（heart_seed）"
+	bundle exec rake heart_seed:xls
+
+	echo "($LINENO)	>> seedファイル 実行"
+	bundle exec rake db:seed
 }
+
 
 # 開発環境のDBバックアップ
 DEV_MYSQL_BK(){
@@ -310,6 +360,89 @@ DEV_MYSQL_BK(){
 	fi
 	cd $rails_dir$app_name
 }
+
+# 開発環境DBバックアップ
+PRO_MYSQL_BK(){
+	echo "($LINENO)	>> DBのバックアップファイルを構築します。"
+	if [ -e ../mysql_bk ]; then
+		# フォルダがある場合
+		echo "($LINENO)	>> フォルダが存在します"
+	else
+		# フォルダがない場合
+		mkdir ../mysql_bk
+	fi
+
+	# production ##########################
+	rails_env="_production"
+	rails_env_bk="_mysql_bk_"
+	all_file="*"
+
+	 mysqldump --user=$mysql_production_user --password="$mysql_production_pass"  $app_name$rails_env > $mysql_bk_dr/$app_name$rails_env$rails_env_bk$(date +%Y%m%d%H%M%S).sql
+
+	cnt=` ls $mysql_bk_dr/$app_name$rails_env$rails_env_bk$all_file | wc -l`
+
+	echo "バックアップ数 = $cnt"
+	echo "バックアップ設定数 = $log_cnt_max"
+
+	#ファイル数が設定値より大きいか比較
+	cd $mysql_bk_dr
+	if [ $cnt -gt $log_cnt_max ]; then
+		echo "($LINENO)	>> development_db 削除"
+		ls -tr $mysql_bk_dr/$app_name$rails_env$rails_env_bk$all_file | head -$(($cnt-$log_cnt_max)) | xargs rm -rf
+	else
+		echo "($LINENO)	>> development_db 保存"
+	fi
+	cd $rails_dir$app_name
+}
+
+########################################################
+# GEM
+########################################################
+# bower インストール
+BOWER_INSTALL(){
+	echo "($LINENO)	>> bower install"
+  bundle exec rake bower:install['--allow-root']
+}
+
+# rails_best_practices 実行
+BEST_PRACTICES(){
+	echo "($LINENO)	>> rails_best_practices 実行します"
+	bundle exec rails_best_practices -f html .
+
+	return 0   #正常終了は戻り値が0
+}
+
+# delayed_job 再起動
+DELAYED_JOB_RESTART(){
+	echo "($LINENO)	>> delayed_job 停止"
+	bundle exec bin/delayed_job -n 2 stop
+
+	echo "($LINENO)	>> delayed_job ジョブクリア"
+	bundle exec rake jobs:clear
+
+	echo "($LINENO)	>> delayed_job 起動"
+	bundle exec bin/delayed_job start
+
+	return 0   #正常終了は戻り値が0
+}
+
+# i18n_generators 実行
+I18N_GENERAT(){
+	echo "($LINENO)	>> i18n_generators 実行"
+	bundle exec rails g i18n ja
+
+	return 0   #正常終了は戻り値が0
+}
+
+# whenever 実行
+WHENEVER_RESTART(){
+	echo "($LINENO)	>> whenever 削除"
+	bundle exec whenever --clear-crontab
+	echo "($LINENO)	>> whenever 登録"
+	bundle exec whenever --update-crontab
+}
+
+
 
 ########################################################
 #   メイン
