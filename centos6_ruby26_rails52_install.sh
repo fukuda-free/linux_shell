44a3f53977961edbc2bbcc73d8fb4f75793725f9
fukuda <fukuda.hiroaki@k-idea.jp>
2019-03-13 18:09:14 +0900

    ruby2.6 + rails5.2.2 のインストールシェルを構築

---
+++ centos6_ruby26_rails52_install.sh
@@ -0,0 +1,122 @@
+#!/bin/sh
+########################################################
+# 環境構築用シェル
+# ruby 2.6
+# rails 5.2.2
+# 作成者  fukuda
+# 更新日  2018/03/20
+########################################################
+
+echoG "開発用パッケージをインストールします"
+DEVELOP_PACKAGE_INSTALL
+
+echoG "時間軸を日本にします"
+sudo ln -sf /usr/share/zoneinfo/Japan /etc/localtime
+date
+sudo yum -y install ntp
+sudo ntpdate ntp.nict.jp
+
+echoG "rbenvのインストール"
+RBENV_INSTALL
+
+echoG 'ruby 2.6.1 install'
+rbenv install -v 2.6.1
+rbenv rehash
+rbenv global 2.6.1
+
+echoG 'ruby のバージョンは以下となります'
+ruby -v
+
+echoG 'rails 5.2.2 install'
+RAILS_INSTALL 5.2.2
+
+
+########################################################
+# メソッド群（TODO：基本弄らない）
+########################################################
+# rails instlal
+RAILS_INSTALL(){
+  gem install rack
+  gem install rails -v  $1
+
+  echoG 'rails のバージョンは以下となります'
+  rails -v
+}
+
+# rbenvのインストール
+RBENV_INSTALL(){
+  # GIT_INSTALL
+  yum install -y git
+
+  git clone git://github.com/sstephenson/rbenv.git /usr/local/src/rbenv
+  echo 'export RBENV_ROOT="/usr/local/src/rbenv"' >> /etc/profile.d/rbenv.sh
+  echo 'export PATH="${RBENV_ROOT}/bin:${PATH}"' >> /etc/profile.d/rbenv.sh
+  echo 'eval "$(rbenv init -)"' >> /etc/profile.d/rbenv.sh
+  source /etc/profile.d/rbenv.sh
+  git clone git://github.com/sstephenson/ruby-build.git /usr/local/src/rbenv/plugins/ruby-build
+  ls /usr/local/src/rbenv/plugins/ruby-build/bin/
+
+  echoG 'rbenv のバージョンは以下となります'
+  rbenv -v
+}
+
+  # 必要パッケージのインストール処理
+DEVELOP_PACKAGE_INSTALL(){
+  echoG 'パッケージを最新にします。パスワードを聞かれることがあります。'
+  sudo -v
+  sudo yum update -y
+  sudo yum install -y zlib
+  sudo yum install -y zlib-devel
+  sudo yum install -y openssl-devel
+  sudo yum install -y sqlite-devel
+  sudo yum install -y gcc-c++
+  sudo yum install -y glibc-headers
+  sudo yum install -y libyaml-devel
+  sudo yum install -y readline
+  sudo yum install -y readline-devel
+  sudo yum install -y libffi-devel
+  sudo yum install -y libxml2
+  sudo yum install -y libxml2-devel
+  sudo yum install -y libxslt
+  sudo yum install -y libxslt-devel
+  sudo yum install -y libyaml-devel
+  sudo yum install -y make
+  sudo yum -y install yum-cron
+}
+
+# echoの装飾用
+ESC="\e["
+ESCEND=m
+COLOR_OFF=${ESC}${ESCEND}
+
+echoW() {
+  # 文字色：Black Bold(灰色)
+  echo -en "${ESC}37;5${ESCEND}"
+  echo "${1}"
+  echo -en "${COLOR_OFF}"
+}
+echoG() {
+  # 文字色：green
+  echo -en "${ESC}32;5${ESCEND}"
+  echo "${1}" | tee -a ${LOG}
+  echo -en "${COLOR_OFF}"
+}
+echoR() {
+  # 文字色：Red
+  echo -en "${ESC}31;5${ESCEND}"
+  echo "${1}" | tee -a ${LOG}
+  echo -en "${COLOR_OFF}"
+}
+echoY() {
+  # 文字色：Yellow
+  echo -en "${ESC}33;5${ESCEND}"
+  echo "${1}" | tee -a ${LOG}
+  echo -en "${COLOR_OFF}"
+}
+echoB() {
+  # 文字色：Yellow
+  echo -en "${ESC}36;5${ESCEND}"
+  echo "${1}" | tee -a ${LOG}
+  echo -en "${COLOR_OFF}"
+}
+########################################################
