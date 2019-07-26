718b2c526abfb606a9e75f7ed5c5d307f38648f6
fukuda <fukuda.hiroaki@k-idea.jp>
2019-07-26 17:12:41 +0900

    mysql チェンジ

--- centOS7/develop_mysql57_install.sh
+++ centOS7/develop_mysql57_install.sh
@@ -5,21 +5,71 @@
 
 ########################################################
 
-# 古いバージョンを削除
-sudo yum -y remove mysql*
-sudo yum -y remove mariadb-libs
+# # 古いバージョンを削除
+# sudo yum -y remove mysql*
+# sudo yum -y remove mariadb-libs
 
-# インストール
-# sudo yum -y localinstall https://dev.mysql.com/get/mysql57-community-release-el7-11.noarch.rpm
+# # インストール
+# # sudo yum -y localinstall https://dev.mysql.com/get/mysql57-community-release-el7-11.noarch.rpm
+# # sudo yum -y install mysql-community-server
+# sudo yum -y installhttps://dev.mysql.com/get/mysql80-community-release-el7-2.noarch.rpm
+# sudo yum-config-manager --disable mysql80-community
+# sudo yum-config-manager --enable mysql57-community
+# rpm -Va --nofiles --nodigest
+# sudo yum -y install "mysql" mysql-community-server mysql-community-devel --skip-broken
+# mysqld --version
+
+# # バージョン確認
+# mysqld --version
+
+# # # MYSQLの設定で、rootのパスワードを「なし」に
+# # echo '' >> /etc/my.cnf
+# # echo 'skip-grant-tables' >> /etc/my.cnf
+
+# # # MYSQLの設定で、文字コードをutf8mb4に
+# # echo 'character-set-server=utf8mb4' >> /etc/my.cnf
+# # echo '' >> /etc/my.cnf
+# # echo '' >> /etc/my.cnf
+# # echo '' >> /etc/my.cnf
+# # echo '[client]' >> /etc/my.cnf
+# # echo 'default-character-set=utf8mb4' >> /etc/my.cnf
+# # echo '' >> /etc/my.cnf
+# # echo '' >> /etc/my.cnf
+
+# # 再起動
+# service mysqld restart
+
+# DB_PASSWORD=$(grep "A temporary password is generated" /var/log/mysqld.log | sed -s 's/.*root@localhost: //')
+# echo "初期パスワードは、「${DB_PASSWORD}」です。"
+# echo "このパスワードは、場合によっては必要となりますので、"
+# echo "メモしておくことをお勧めします"
+# echo ""
+
+# sudo systemctl start mysqld.service
+# sudo systemctl enable mysqld.service
+# systemctl enable mysqld.service
+# systemctl start mysqld.service
+
+# echo "現在のMYSQLのバージョンは、以下の通りです"
+# mysqld --version
+# echo ''
+
+
+
+# --------------------------------------------------------------
+# # 事前準備
+# sudo yum -y remove mariadb-libs
+# sudo yum -y remove mysql*
+# sudo rm -rf /var/lib/mysql/
+
+# # MySQL 公式 yum リポジトリの追加
+# sudo yum -y localinstall http://dev.mysql.com/get/mysql57-community-release-el7-7.noarch.rpm
+
+# # インストール
 # sudo yum -y install mysql-community-server
-sudo yum -y installhttps://dev.mysql.com/get/mysql80-community-release-el7-2.noarch.rpm
-sudo yum-config-manager --disable mysql80-community
-sudo yum-config-manager --enable mysql57-community
-sudo yum -y install "mysql" mysql-community-server mysql-community-devel
-mysqld --version
 
-# バージョン確認
-mysqld --version
+# # バージョン確認
+# mysqld --version
 
 # # MYSQLの設定で、rootのパスワードを「なし」に
 # echo '' >> /etc/my.cnf
@@ -35,61 +85,42 @@ mysqld --version
 # echo '' >> /etc/my.cnf
 # echo '' >> /etc/my.cnf
 
-# 再起動
-service mysqld restart
+# # 自動起動＋起動
+# sudo systemctl enable mysqld.service
+# sudo systemctl start mysqld.service
 
-DB_PASSWORD=$(grep "A temporary password is generated" /var/log/mysqld.log | sed -s 's/.*root@localhost: //')
-echo "初期パスワードは、「${DB_PASSWORD}」です。"
-echo "このパスワードは、場合によっては必要となりますので、"
-echo "メモしておくことをお勧めします"
-echo ""
+# # バージョン確認
+# MYSQL_V=$(mysqld --version)
+# echo '-------------------------------------------'
+# echo '現在のMYSQLのバージョンは、以下の通りです'
+# mysqld --version
+# echo '-------------------------------------------'
 
-sudo systemctl start mysqld.service
-sudo systemctl enable mysqld.service
-systemctl enable mysqld.service
-systemctl start mysqld.service
 
-echo "現在のMYSQLのバージョンは、以下の通りです"
-mysqld --version
-echo ''
+# 3 ----------------------------------------------------
 
 
 
 
-# 事前準備
-sudo yum -y remove mariadb-libs
-sudo yum -y remove mysql*
-sudo rm -rf /var/lib/mysql/
 
-# MySQL 公式 yum リポジトリの追加
-sudo yum -y localinstall http://dev.mysql.com/get/mysql57-community-release-el7-7.noarch.rpm
+# 不要部分を削除
+yum remove mariadb-libs
+yum remove mysql*
+rm -rf /var/lib/mysql/*
+yum clean all
 
-# インストール
-sudo yum -y install mysql-community-server
 
-# バージョン確認
-mysqld --version
+# yumリポジトリel7をインストール
+yum install https://dev.mysql.com/get/mysql80-community-release-el7-x.noarch.rpm
+# sudo rpm -Uvh http://dev.mysql.com/get/mysql57-community-release-el7-7.noarch.rpm
 
-# MYSQLの設定で、rootのパスワードを「なし」に
-echo '' >> /etc/my.cnf
-echo 'skip-grant-tables' >> /etc/my.cnf
 
-# MYSQLの設定で、文字コードをutf8mb4に
-echo 'character-set-server=utf8mb4' >> /etc/my.cnf
-echo '' >> /etc/my.cnf
-echo '' >> /etc/my.cnf
-echo '' >> /etc/my.cnf
-echo '[client]' >> /etc/my.cnf
-echo 'default-character-set=utf8mb4' >> /etc/my.cnf
-echo '' >> /etc/my.cnf
-echo '' >> /etc/my.cnf
+# mysqlインストール
+sudo yum -y update
+sudo yum -y install mysql-community-server mysql-community-devel --skip-broken
 
-# 自動起動＋起動
-sudo systemctl enable mysqld.service
-sudo systemctl start mysqld.service
 
-# バージョン確認
-MYSQL_V=$(mysqld --version)
+# 確認
 echo '-------------------------------------------'
 echo '現在のMYSQLのバージョンは、以下の通りです'
 mysqld --version
