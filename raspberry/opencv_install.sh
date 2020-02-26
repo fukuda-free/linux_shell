b86410d0eaf6a6bee3c1bc734e4ec89c37f6bacc
fukuda <fukuda.hiroaki@k-idea.jp>
2020-02-26 18:11:08 +0900

    raspberry pi 用のシェルを準備

---
+++ raspberry/opencv_install.sh
@@ -0,0 +1,17 @@
+sudo apt-get install -y libopencv-dev
+sudo apt-get install -y python-opencv
+python -V
+python -c "import cv2; print(cv2.__version__)"
+
+
+sudo apt-get install -y build-essential cmake pkg-config
+sudo apt-get install -y libjpeg-dev libtiff5-dev libjasper-dev libpng-dev
+sudo apt-get install -y libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
+sudo apt-get install -y libxvidcore-dev libx264-dev
+sudo apt-get install -y libatlas-base-dev gfortran
+sudo apt-get install -y libhdf5-dev libhdf5-serial-dev libhdf5-103
+sudo apt-get install -y libqtgui4 libqtwebkit4 libqt4-test python3-pyqt5
+sudo apt-get install -y python3-dev
+pip3 install opencv-contrib-python==4.1.0.25
+python3 -V
+python3 -c "import cv2; print(cv2.__version__)"
---
+++ raspberry/os_update.sh
@@ -0,0 +1,9 @@
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt dist-upgrade -y
+sudo apt autoremove -y
+sudo apt autoclean -y
+sudo apt install -y jfbterm
+sudo apt install -y ibus-anthy
+sudo apt install -y vim
+sudo reboot
