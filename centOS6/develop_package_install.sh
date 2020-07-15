e129c386d7d86e9c834993ea1b86cdca32588ff2
fukuda <fukuda.hiroaki@k-idea.jp>
2020-07-15 12:43:47 +0900

    centos6用を修正中

--- centOS6/develop_package_install.sh
+++ centOS6/develop_package_install.sh
@@ -29,6 +29,27 @@ sudo yum install -y curl
 sudo yum install -y wget
 sudo yum install -y git
 
+echo "(${LINENO})  >> スワップ領域を自動で割り当てます"
+echo "現在のスワップ領域は以下の通りです"
+free
+
+SWAPFILENAME=/swap.img
+MEMSIZE=`cat /proc/meminfo | grep MemTotal | awk '{print $2}'`
+
+if [ $MEMSIZE -lt 2097152 ]; then
+  SIZE=$((MEMSIZE * 2))k
+elif [ $MEMSIZE -lt 8388608 ]; then
+  SIZE=${MEMSIZE}k
+elif [ $MEMSIZE -lt 67108864 ]; then
+  SIZE=$((MEMSIZE / 2))k
+else
+  SIZE=4194304k
+fi
+
+fallocate -l $SIZE $SWAPFILENAME && mkswap $SWAPFILENAME && swapon $SWAPFILENAME
+
+echo "スワップ領域を以下に設定しました"
+free
 
 echo "時間軸を日本にします"
 sudo ln -sf /usr/share/zoneinfo/Japan /etc/localtime
